359d1a27d17b37ac3aec2ef11af6d72c4c41dc0f
CreedsCode <38018790+CreedsCode@users.noreply.github.com>
2018-08-02 13:00:57 +0200

    End of WOrk

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Contact extends Component {
   render() {
     return (
       <div>
-        <h1>Contact Page</h1>
+        <h1>
+Contact Page
+</h1>
       </div>
     );
   }

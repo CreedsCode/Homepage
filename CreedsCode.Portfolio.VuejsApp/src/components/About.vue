bfbb88c2ac9f7ab6808e018e4fc89f3ce881a489
CreedsCode <hoffmann.dercio@hotmail.com>
2019-05-12 11:54:01 +0200

    Base files Import

---
+++ CreedsCode.Portfolio.VuejsApp/src/components/About.vue
@@ -0,0 +1,63 @@
+﻿<template>
+    <div>
+
+    <header>
+        <div class="menu-btn">
+            <div class="btn-line"></div>
+            <div class="btn-line"></div>
+            <div class="btn-line"></div>
+        </div>
+
+        <nav class="menu">
+            <div class="menu-branding">
+                <div class="portrait"></div>
+            </div>
+            <ul class="menu-nav">
+                <li class="nav-item ">
+                    <a href="index.html" class="nav-link">
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item current">
+                    <a href="about.html" class="nav-link">
+                        About Me
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="work.html" class="nav-link">
+                        My Work
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+    </header>
+    <main id="about">
+        <h1 class="lg-heading">About <span class="text-secondary">Me</span></h1>
+        <h2 class="sm-heading">Let me tell you a few things...</h2>
+        <div class="about-info">
+            <img src="img/portrait.jpg" alt="Logo" class="bio-image">
+            <div class="bio">
+                <h3 class="text-secondary">BIO</h3>
+                <p>Hey, I am Dercio. I am 15 years old, from Germany and I have a lot of fun in programming. I try to realize
+                    projects in most areas of software development. Especially in the area of JavaScript, but also in many
+                    other areas.</p>
+            </div>
+        </div>
+    </main>
+
+    <footer id="main-footer">Copyright &copy; 2018</footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'About',
+        props: {
+            msg: String
+        }
+    };
+</script>
+
+<style scoped>
+</style>
--- CreedsCode.Portfolio.VuejsApp/src/components/Home.vue
+++ CreedsCode.Portfolio.VuejsApp/src/components/Home.vue
@@ -1,7 +1,45 @@
 <template>
-    <div class="home">
-        <h1>{{ msg }}</h1>
-        <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a>.</p>
+    <div>
+        <header class="home">
+            <div class="menu-btn">
+                <div class="btn-line"></div>
+                <div class="btn-line"></div>
+                <div class="btn-line"></div>
+            </div>
+
+            <nav class="menu">
+                <div class="menu-branding">
+                    <div class="portrait"></div>
+                </div>
+                <ul class="menu-nav">
+                    <li class="nav-item current">
+                        <a href="" class="nav-link">
+                            Home
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="about.html" class="nav-link">
+                            About Me
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="work.html" class="nav-link">
+                            My Work
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main id="home">
+            <h1 class="lg-heading">Creeds <span class="text-secondary">Code</span></h1>
+            <h2 class="sm-heading">Hobby Programmer.</h2>
+            <div class="icons">
+                <a href="https://twitter.com/creedgamesoa"><i class="fab fa-twitter fa-2x"></i></a>
+                <a href="https://github.com/CreedsCode/"><i class="fab fa-github fa-2x"></i></a>
+                <a href="https://www.instagram.com/creedscode/"><i class="fab fa-instagram fa-2x"></i></a>
+                <a href="https://www.youtube.com/channel/UCnZh8MLnCOugySmVjay55dA"><i class="fab fa-youtube fa-2x"></i></a>
+            </div>
+        </main>
     </div>
 </template>
 

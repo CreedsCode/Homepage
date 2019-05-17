64ccd73c5e8054cf378017d1768287430c3494f9
CreedsCode <hoffmann.dercio@hotmail.com>
2019-05-17 19:21:06 +0200

    Base component

---
+++ CreedsCode.Portfolio.VuejsApp/src/components/SocialIcons.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<div v-for="(item,index) in testlistItems" :key="index" class="icons">
+				<a :id="item.url"><i :class="item.faIcon"></i></a>
+		</div>
+	</div>
+</template>
+
+<script>
+    export default {
+        name: "SocialIcons",
+        data() {
+            return {
+				testlistItems: [
+					{
+						name:"Twitter",
+						url:"https://twitter.com/creedgamesoa",
+						faIcon:"fa-twitter fab fa-2x "
+					},
+					{
+						name:"GitHub",
+						url:"https://github.com/CreedsCode/",
+						faIcon:"fa-github fab fa-2x "
+					},
+					{
+						name:"Instagram",
+						url:"https://www.instagram.com/creedscode/",
+						faIcon:"fa-instagram fab fa-2x "
+					},
+					{
+						name:"Youtube",
+						url:"https://www.youtube.com/channel/UCnZh8MLnCOugySmVjay55dA",
+						faIcon:"fa-youtube fab fa-2x "
+					}
+				],
+            };
+        },
+        props: {
+            listItems: {
+                type:  Array,
+				// required:true
+                required:false
+				
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>

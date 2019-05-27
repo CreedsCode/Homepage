6260e6176338cb4c847145fb12a8623b3215638a
CreedsCode <hoffmann.dercio@hotmail.com>
2019-05-28 00:24:20 +0200

    add Online Profiles

---
+++ CreedsCode.Portfolio.BackService/Data/ApiDataContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CreedsCode.Portfolio.BackService.Data
+{
+    public class ApiContext : DbContext
+    {
+        
+    }
+}
---
+++ CreedsCode.Portfolio.BackService/Models/OnlineProfile.cs
@@ -0,0 +1,9 @@
+namespace CreedsCode.Portfolio.BackService.Models
+{
+    public class OnlineProfiles
+    {
+        public int ID { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+    }
+}
